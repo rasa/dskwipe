9ed739449cd2a24446a4ffae21a64ac56b36c7c5
bilogic <>
2021-02-17 09:32:39 +0800

    added getopt.c and header from https://www.codeproject.com/Articles/157001/Full-getopt-Port-for-Unicode-and-Multibyte-Microso

--- version.h
+++ version.h
@@ -5,5 +5,15 @@
 #define VER_MAJOR				1
 #define VER_MINOR				3
 #define VER_STRING2				"1.3"
+#define VER_FILE_VERSION        1,3,0,0
+#define VER_PRODUCT_VERSION     1,3,0,0
 
-#include "ver_defaults.h"
+#define VER_COMMENTS "\0"
+#define VER_COMPANY_NAME "\0"
+#define VER_FILE_STRING "\0"
+#define VER_LEGAL_COPYRIGHT "\0"
+#define VER_ORIGINAL_FILENAME "\0"
+#define VER_PRODUCT_NAME "\0"
+#define VER_PRODUCT_STRING "\0"
+
+//#include "ver_defaults.h"
